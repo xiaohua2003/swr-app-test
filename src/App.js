dc3daef388745223197121d51d88f455a4f32454
Xiaohua Cai <xcai3@ualberta.ca>
2023-01-04 07:25:14 -0800

    update code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react';
+
+
+const UserName = ({username}) => {
+  return (<h4> user name: {username}</h4>)
+}
+
+const Email = () => {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()).then(data => setUser(data))
+  }, [])
+  return (
+    <div>
+      {user && user?.map((item) => {
+        return  <h4  key={item.id}> email: {item.email}</h4>
+      })}
+    
+    </div>
+)
+}
+const Name = ({name}) => {
+  return (<h4>Name: {name}</h4>)
+}
 
 function App() {
+  // const [user, setUser] = useState(null);
+  // useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()).then(data => setUser(data))
+  // }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Email/>
+      {/* {
+       user && user?.map((item) => {
+          return (<div key={item.id}>
+             <UserName username={item?.username}/>
+             <UserName username={item?.username}/>
+             <Email />
+             <Name name={item.name}/>
+             <hr/>
+          
+
+          </div>)
+        })
+      } */}
+   
+
     </div>
   );
 }
---
+++ src/App2.js
@@ -0,0 +1,67 @@
+import {useEffect, useState} from 'react';
+import useSWR from 'swr'
+
+const UserName = ({username}) => {
+  return (<h4> user name: {username}</h4>)
+}
+
+const Email = () => {
+const fetcher = (url) => fetch(url).then(res => res.json())
+const {data, error, isLoading} = useSWR('https://jsonplaceholder.typicode.com/users', fetcher)
+//   const [user, setUser] = useState(null);
+//   useEffect(() => {
+//     fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()).then(data => setUser(data))
+//   }, [])
+  return (
+    // <div>
+    //   {user && user?.map((item) => {
+    //     return  <h4  key={item.id}> email: {item.email}</h4>
+    //   })}
+    
+    // </div>
+    <div>
+      {data && data?.map((item) => {
+        return  <h4  key={item.id}> email: {item.email}</h4>
+      })}
+    
+    </div>
+)
+}
+const Name = () => {
+//     const [user, setUser] = useState(null);
+//   useEffect(() => {
+//     fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()).then(data => setUser(data))
+//   }, [])
+const fetcher = (url) => fetch(url).then(res => res.json())
+const {data, error, isLoading} = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
+  return (
+    // <div>
+    //   {user && user?.map((item) => {
+    //     return  <h4  key={item.id}> email: {item.name}</h4>
+    //   })}
+    
+    // </div>
+     <div>
+     {data && data?.map((item) => {
+       return  <h4  key={item.id}> title: {item.title}</h4>
+     })}
+   
+   </div>
+)
+}
+
+function App() {
+  // const [user, setUser] = useState(null);
+  // useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()).then(data => setUser(data))
+  // }, [])
+
+  return (
+    <div className="App">
+      <Email/>
+      <Name/>
+    </div>
+  );
+}
+
+export default App;
